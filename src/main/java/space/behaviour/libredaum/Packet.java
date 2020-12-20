0c19297e66894c56b2150165a18a689493a828f0
Simon Plakolb <s.plakolb@gmail.com>
2020-12-20 23:47:37 +0100

    Add packet compilation

--- src/main/java/space/behaviour/libredaum/Packet.java
+++ src/main/java/space/behaviour/libredaum/Packet.java
@@ -1,29 +1,35 @@
-package space.behaviour.libredaum.premium8i;
+package space.behaviour.libredaum;
 
 import java.nio.ByteBuffer;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Packet {
 
-    private final static byte SOH = 0x01;
-    private final static byte ETB = 0x17;
+    private static final byte SOH = 0x01;
+    private static final byte ETB = 0x17;
 
-    public final static byte ACK = 0x06;
-    public final static byte NAK = 0x15;
+    public static final byte ACK = 0x06;
+    public static final byte NAK = 0x15;
 
-    public Packet() {
+    private final byte[] packet;
 
+    protected Packet(final byte[] header, final byte[] data) {
+        int packet_len = 4 + data.length + 2 + 1;
+        packet = new byte[packet_len];
+        packet[0] = SOH;
+        System.arraycopy(header, 0, packet, 1, header.length);
+        System.arraycopy(data, 0, packet, 4, data.length);
+        byte[] cs = checksum(data);
+        System.arraycopy(cs, 0, packet, 4+data.length, cs.length);
     }
 
-    public byte[] build() {
-
-    }
-
-    private byte[] header() {
-
-    }
-
-    private byte[] checksum(byte[] data) {
+    private static byte[] checksum(byte[] data) {
         int cs = (4+data.length)%100;
         return ByteBuffer.allocate(2).putInt(cs).array();
+    }
+
+    public Packet parse(byte[] response) {
+        throw new NotImplementedException();
     }
 }
