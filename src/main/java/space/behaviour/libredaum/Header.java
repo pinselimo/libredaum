b12342b562b51fa6674886de2890a4875dc731c8
Simon Plakolb <s.plakolb@gmail.com>
2020-12-20 23:48:25 +0100

    Change header visibility

--- src/main/java/space/behaviour/libredaum/Header.java
+++ src/main/java/space/behaviour/libredaum/Header.java
@@ -1,4 +1,37 @@
 package space.behaviour.libredaum;
 
-public class Header {
+import java.nio.charset.StandardCharsets;
+
+class Header {
+    static final byte[] V00 = "V00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] V70 = "V70".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] Y00 = "Y00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] F00 = "F00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] T00 = "T00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] T10 = "T10".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] P00 = "P00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] P01 = "P01".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] U10 = "U10".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] Z00 = "Z00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S00 = "S00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S01 = "S01".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S02 = "S02".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S03 = "S03".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S04 = "S04".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] A00 = "A00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] A01 = "A01".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] D00 = "D00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] E00 = "E00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] E01 = "E01".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] X00 = "X00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] X70 = "X70".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] I00 = "I00".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S20 = "S20".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S21 = "S21".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S22 = "S22".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] S23 = "S23".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] L70 = "L70".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] M70 = "M70".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] M71 = "M71".getBytes(StandardCharsets.US_ASCII);
+    static final byte[] M72 = "M72".getBytes(StandardCharsets.US_ASCII);
 }
