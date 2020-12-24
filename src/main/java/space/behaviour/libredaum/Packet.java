d2ef6f5a1b3231f7ace5f7ffc34bfc07d7e3406a
Simon Plakolb <s.plakolb@gmail.com>
2020-12-24 16:40:44 +0100

    Fix checksum calculation

--- src/main/java/space/behaviour/libredaum/Packet.java
+++ src/main/java/space/behaviour/libredaum/Packet.java
@@ -1,12 +1,12 @@
 package space.behaviour.libredaum;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class Packet {
 
-    private static final byte SOH = 0x01;
-    private static final byte ETB = 0x17;
-
+    public static final byte SOH = 0x01;
+    public static final byte ETB = 0x17;
     public static final byte ACK = 0x06;
     public static final byte NAK = 0x15;
 
@@ -18,13 +18,21 @@ public class Packet {
         bytes[0] = SOH;
         System.arraycopy(header, 0, bytes, 1, header.length);
         System.arraycopy(data, 0, bytes, 4, data.length);
-        byte[] cs = checksum(data);
+        byte[] cs = checksum(header, data);
         System.arraycopy(cs, 0, bytes, 4+data.length, cs.length);
         bytes[packet_len-1] = ETB;
     }
 
-    static byte[] checksum(byte[] data) {
-        int cs = (4+data.length)%100;
-        return ByteBuffer.allocate(2).putInt(cs).array();
+    static byte[] checksum(byte[] header, byte[] data) {
+        byte[] cs = new byte[header.length + data.length];
+        System.arraycopy(header, 0 , cs, 0, header.length);
+        System.arraycopy(data, 0, cs, header.length, data.length);
+
+        int sum = 0;
+        for (byte b : cs) {
+            sum += b;
+        }
+        sum %= 100;
+        return Integer.toString(sum).getBytes(StandardCharsets.US_ASCII);
     }
 }
