d919058d9c5fbdc1896d8cf74c82f997bd63765f
Simon Plakolb <s.plakolb@gmail.com>
2020-12-24 17:16:57 +0100

    Add connection provider main class

---
+++ src/main/java/space/behaviour/libredaum/ConnectionProvider.java
@@ -0,0 +1,120 @@
+package space.behaviour.libredaum;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ConnectionProvider implements ResponseHandler.ConnectionListener  {
+    private Socket socket;
+    private Thread inputThread;
+    private OutputStream outputStream;
+
+    private Packet lastPacketSent = null;
+
+    public interface ConnectionUpdateListener {
+        void onConnected();
+        void onConnectionError(Exception e);
+        void onDisconnected();
+    }
+
+    public interface ResponseListener {
+        boolean onResponse(Response response);
+    }
+
+    private final ConnectionUpdateListener connectionUpdateListener;
+    private final ResponseListener responseListener;
+
+    public ConnectionProvider(ConnectionUpdateListener connectionListener, ResponseListener responseListener) {
+        this.connectionUpdateListener = connectionListener;
+        this.responseListener = responseListener;
+    }
+
+    public boolean sendPacket(Packet packet) {
+
+        try {
+            outputStream.write(packet.bytes);
+            lastPacketSent = packet;
+            return true;
+        } catch (IOException e) {
+            connectionUpdateListener.onConnectionError(e);
+            return false;
+        }
+    }
+
+    public void close() {
+        if (inputThread.isAlive()) inputThread.interrupt();
+        try {
+            this.socket.close();
+
+        } catch (IOException e) {
+            connectionUpdateListener.onConnectionError(e);
+        }
+    }
+
+    private void connect(final InetSocketAddress daum)
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Socket sock = new Socket();
+                try
+                {
+                    sock.connect(daum);
+                    onSocket(sock);
+                }
+                catch (IOException e)
+                {
+                    connectionUpdateListener.onConnectionError(e);
+                }
+            }
+        }).start();
+    }
+
+    private void onSocket(final Socket socket) {
+
+        this.socket = socket;
+        try
+        {
+            this.outputStream = socket.getOutputStream();
+
+            inputThread = new Thread(new ResponseHandler(socket.getInputStream(), this.responseListener, this));
+            inputThread.start();
+
+            connectionUpdateListener.onConnected();
+        }
+        catch (IOException e)
+        {
+            connectionUpdateListener.onConnectionError(e);
+        }
+    }
+
+    @Override
+    public void requireNAK() {
+        try {
+            outputStream.write(Packet.NAK);
+        } catch (IOException e) {
+            connectionUpdateListener.onConnectionError(e);
+        }
+    }
+
+    @Override
+    public void requireACK() {
+        try {
+            outputStream.write(Packet.ACK);
+        } catch (IOException e) {
+            connectionUpdateListener.onConnectionError(e);
+        }
+    }
+
+    @Override
+    public void receivedNAK() {
+        try {
+            outputStream.write(lastPacketSent.bytes);
+        } catch (IOException e) {
+            connectionUpdateListener.onConnectionError(e);
+        }
+    }
+}
